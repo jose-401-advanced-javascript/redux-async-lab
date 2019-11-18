9fee50cce0d44e1775be3cad442095439399818f
Jose <j.a.ojeda@live.com>
2019-11-18 15:25:58 -0800

    set up store.js and tied it to index.js and App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import DisplayCharacters from '../containers/DisplayCharacters';
 
 export default function App() {
   
   return (
-    <h1>Hello World</h1>
+    <DisplayCharacters />
   );
 }
