dc01d1127bbb3aa69b14fd1ee4ccc27cd5f9d234
Jose <j.a.ojeda@live.com>
2019-11-18 16:58:34 -0800

    updated function name to avoid confustion

--- src/actions/charactersActions.js
+++ src/actions/charactersActions.js
@@ -2,8 +2,8 @@ import { fetchCharacters } from '../services/avatarApi';
 
 export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
 
-export const getCharacters = () => dispatch => {
-  fetchCharacters()
+export const updateCharacters = () => dispatch => {
+  return fetchCharacters()
     .then(characters => {
       dispatch({
         type: FETCH_CHARACTERS,
