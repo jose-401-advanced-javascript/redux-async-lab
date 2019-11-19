d5f10c8702d584ea0421ce683a3f514621772b7a
Jose <j.a.ojeda@live.com>
2019-11-18 17:46:08 -0800

    added actions for loading

--- src/actions/charactersActions.js
+++ src/actions/charactersActions.js
@@ -1,13 +1,23 @@
 import { fetchCharacters } from '../services/avatarApi';
 
 export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
+export const FETCH_CHARACTERS_LOADING = 'FETCH_CHARACTERS_LOADING';
+export const FETCH_CHARACTERS_DONE = 'FETCH_CHARACTERS_DONE';
 
 export const updateCharacters = () => dispatch => {
-  return fetchCharacters()
+  dispatch({
+    type: FETCH_CHARACTERS_LOADING
+  });
+
+  fetchCharacters()
     .then(characters => {
       dispatch({
         type: FETCH_CHARACTERS,
         payload: characters
+      });
+
+      dispatch({
+        type: FETCH_CHARACTERS_DONE
       });
     });
 };
