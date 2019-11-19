e5cf883f0fb944dd3efbef7fc5879f828b200ea6
Jose <j.a.ojeda@live.com>
2019-11-18 16:59:36 -0800

    added fetchCharacters action to reducer

--- src/reducers/charactersReducer.js
+++ src/reducers/charactersReducer.js
@@ -1,9 +1,13 @@
+import { FETCH_CHARACTERS } from '../actions/charactersActions';
+
 const initialState = {
   characters: []
 };
 
 export default function reducer(state = initialState, action) {
   switch(action.type) {
+    case FETCH_CHARACTERS:
+      return { ...state, characters: [...action.payload] };
     default:
       return state;
   }
