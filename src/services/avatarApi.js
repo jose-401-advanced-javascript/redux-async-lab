37f51c9a4ea487dc76939df6d358dff959b723c4
Jose <j.a.ojeda@live.com>
2019-11-18 16:57:48 -0800

    updated fetchCharacters

--- src/services/avatarApi.js
+++ src/services/avatarApi.js
@@ -1,4 +1,4 @@
-export const fetchCharacters = (page) => {
-  return fetch(`https://last-airbender-api.herokuapp.com/api/v1/characters?page=${page}`)
+export const fetchCharacters = () => {
+  return fetch('https://last-airbender-api.herokuapp.com/api/v1/characters')
     .then(results => results.json());
 };
