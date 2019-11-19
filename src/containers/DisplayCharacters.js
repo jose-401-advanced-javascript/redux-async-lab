fe1a3d9f95685a76c8caae08b1358a7836d2c005
Jose <j.a.ojeda@live.com>
2019-11-18 17:47:01 -0800

    added loading to container

--- src/containers/DisplayCharacters.js
+++ src/containers/DisplayCharacters.js
@@ -1,16 +1,19 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { updateCharacters } from '../actions/charactersActions';
-import { getCharacters } from '../selectors/charactersSelectors';
+import { getCharacters, getCharactersLoading } from '../selectors/charactersSelectors';
 import Characters from '../components/Characters';
 
 const DisplayCharacters = () => {
   const characters = useSelector(getCharacters);
+  const loading = useSelector(getCharactersLoading);
   const dispatch = useDispatch();
   
   useEffect(() => {
     dispatch(updateCharacters());
   }, []);
+
+  if(loading) return <h1>Loading...</h1>;
 
   return (
     <Characters characters={characters} />
