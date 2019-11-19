9a4c5ff040db9f861c339caa0397202653e2f7b1
Jose <j.a.ojeda@live.com>
2019-11-19 07:38:33 -0800

    updated actions to use promise-middleware

--- src/actions/charactersActions.js
+++ src/actions/charactersActions.js
@@ -1,8 +1,9 @@
 import { fetchCharacters } from '../services/avatarApi';
+import { createAction } from 'promise-middleware-redux';
 
-export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
-export const FETCH_CHARACTERS_LOADING = 'FETCH_CHARACTERS_LOADING';
-export const FETCH_CHARACTERS_DONE = 'FETCH_CHARACTERS_DONE';
+// export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
+// export const FETCH_CHARACTERS_LOADING = 'FETCH_CHARACTERS_LOADING';
+// export const FETCH_CHARACTERS_DONE = 'FETCH_CHARACTERS_DONE';
 
 export const updateCharacters = () => dispatch => {
   dispatch({
@@ -21,3 +22,17 @@ export const updateCharacters = () => dispatch => {
       });
     });
 };
+
+// const updateCharactersPromise = () => ({
+//   type: FETCH_CHARACTERS,
+//   pendingType: FETCH_CHARACTERS_LOADING,
+//   fulfilledType: FETCH_CHARACTERS_DONE,
+//   payload: fetchCharacters()
+// });
+
+export const [
+  updateCharactersPromise,
+  FETCH_CHARACTERS,
+  FETCH_CHARACTERS_LOADING,
+  FETCH_CHARACTERS_DONE
+] = createAction('FETCH_CHARACTERS', fetchCharacters);
