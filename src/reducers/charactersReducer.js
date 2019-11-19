2cec6438fcebc14038706b22efb109f6a031e944
Jose <j.a.ojeda@live.com>
2019-11-18 17:46:33 -0800

    added cases for loading and added loading to state

--- src/reducers/charactersReducer.js
+++ src/reducers/charactersReducer.js
@@ -1,13 +1,18 @@
-import { FETCH_CHARACTERS } from '../actions/charactersActions';
+import { FETCH_CHARACTERS, FETCH_CHARACTERS_LOADING, FETCH_CHARACTERS_DONE } from '../actions/charactersActions';
 
 const initialState = {
-  characters: []
+  characters: [],
+  loading: true
 };
 
 export default function reducer(state = initialState, action) {
   switch(action.type) {
     case FETCH_CHARACTERS:
       return { ...state, characters: [...action.payload] };
+    case FETCH_CHARACTERS_LOADING:
+      return { ...state, loading: true };
+    case FETCH_CHARACTERS_DONE:
+      return { ...state, loading: false };
     default:
       return state;
   }
